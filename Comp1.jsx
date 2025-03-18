285927870a83e9b1c32be1a1e054525b800a5c2d
thanusha906 <tduggireddyBTECH22@ced.alliance.edu.in>
2025-03-18 16:38:00 +0530

    Add files via upload

---
+++ Comp1.jsx
@@ -0,0 +1,9 @@
+function Comp1 (){
+    return (
+    <div>
+        <h1>Hello World</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque cumque saepe laudantium rerum, deserunt aliquam, aut eum suscipit quod rem dolore corporis dignissimos distinctio? Omnis saepe nulla amet laborum libero.</p>
+        </div>
+        );
+}
+export default Comp1;
